44a3e95e5b10cc17b6a9f7a57ebad7a507f2ee92
lucy5940 <lpy0573@gmail.com>
2022-01-29 11:26:53 -0500

    portfolio_project

---
+++ jobs/models.py
@@ -0,0 +1,9 @@
+from distutils.command.upload import upload
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    # image
+    image = models.ImageField(upload_to="images/")
+    # summary
+    summary = models.CharField(max_length=200)
